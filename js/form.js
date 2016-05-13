a75431e8092f0a14003e416789478aa038819e57
SahiyaGus <sahiyag@yandex.ru>
2016-05-13 12:48:38 +0300

    анимация формы

--- js/form.js
+++ js/form.js
@@ -2,10 +2,7 @@ var link = document.querySelector(".search-hotels");
 var search = document.querySelector(".search-form");
 var close = document.querySelector(".search-form-close");
 var form = search.querySelector("form");
-var datein = search.querySelector("[name=check-in]");
-var dateout = search.querySelector("[name=check-out]");
-var adults = search.querySelector("[name=adults]");
-var children = search.querySelector("[name=children]");
+
 
 link.addEventListener("click", function(event){
   event.preventDefault();
@@ -18,7 +15,7 @@ close.addEventListener("click", function(event) {
   search.classList.remove("search-form-show");
 });
 
-window.addEventListener("keydow", function(event) {
+window.addEventListener("keydown", function(event) {
   if (event.keyCode === 27) {
     if (search.classList.contains("search-form-show")) {
       search.classList.remove("search-form-show");
