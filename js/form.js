ac462d2eff87758e2170e65e445eac30b1c6741f
SahiyaGus <sahiyag@yandex.ru>
2016-05-13 14:01:54 +0300

    скрипт для формы скрыть-показать

--- js/form.js
+++ js/form.js
@@ -1,18 +1,11 @@
 var link = document.querySelector(".search-hotels");
 var search = document.querySelector(".search-form");
-var close = document.querySelector(".search-form-close");
 var form = search.querySelector("form");
-
 
 link.addEventListener("click", function(event){
   event.preventDefault();
-  search.classList.add("search-form-show");
+  search.classList.toggle("search-form-show");
   date.focus();
-});
-
-close.addEventListener("click", function(event) {
-  event.preventDefault();
-  search.classList.remove("search-form-show");
 });
 
 window.addEventListener("keydown", function(event) {
